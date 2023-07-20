5e26c0827eb430e1c3a0a4eef0cddafaf8e16140
david-YJ-Kim <yj.david.kim@gmail.com>
2023-07-20 20:56:23 +0900

    Tweek for Spring Agent
    URI change for Spring Agent.
    Logger for node js.

--- src/server.js
+++ src/server.js
@@ -3,23 +3,56 @@ const cors = require("cors");
 const router = express.Router();
 const fs = require("fs");
 const app = express();
+const logger = require('./logger'); // Adjust the path accordingly based on the location of logger.js
+
+const baseFile = '../sellter_server/static/build/springVersion/'
+
 
 app.use(cors());
 
-app.get("/download/agent/browser", (req, res, next) => {
-  console.log("req IN");
+app.get("/download/spring/agent/jdk", (req, res, next) => {
 
-  filePath = "../static/build/sellter_agent.exe";
-  fileName = "agent.exe";
-  const stream = fs.createReadStream(filePath);
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
+
+  fileName = "openJdk11.zip";
+  const stream = fs.createReadStream(baseFile + fileName);
   res.setHeader("Content-Disposition", `attachment; filename=${fileName}`);
   stream.pipe(res);
 
   console.log("res OUT");
 });
 
+app.get("/download/spring/agent/ap", (req, res, next) => {
+
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
+
+  fileName = "sellter.agent-0.0.1-SNAPSHOT.jar";
+  installName = "sellterAgent.jar"
+  const stream = fs.createReadStream(baseFile + fileName);
+  res.setHeader("Content-Disposition", `attachment; filename=${installName}`);
+  stream.pipe(res);
+
+  console.log("res OUT");
+});
+
+
 app.get("/download/agent", (req, res, next) => {
-  console.log("req IN");
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
 
   filePath = "../static/build/sellter_agent.exe";
   fileName = "agent.exe";
@@ -30,7 +63,12 @@ app.get("/download/agent", (req, res, next) => {
 });
 
 app.get("/download/source", (req, res, next) => {
-  console.log("req IN");
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
 
   filePath = "../static/build/source.zip";
   fileName = "source.zip";
@@ -41,7 +79,12 @@ app.get("/download/source", (req, res, next) => {
 });
 
 app.get("/download/packageJson", (req, res, next) => {
-  console.log("req IN");
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
 
   filePath = "../static/build/package.json";
   fileName = "package.json";
@@ -55,13 +98,34 @@ app.get("/download/packageJson", (req, res, next) => {
 });
 
 app.get("/download/regiAdd", (req, res, next) => {
-  console.log("req IN");
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
 
   // filePath = "D:/Workspace/js/sellter_demo_server_v0/static/build/regiAdd_startProgram.bat";
   filePath = "../static/build/regiAdd_startProgram.bat";
   fileName = "regiAdd_startProgram.bat";
   const data = fs.readFileSync(filePath, "utf8");
   res.send(data);
+
+  // res.setHeader("Content-type", `some/type`);
+  // fs.createReadStream(filePath).pipe(res);
+
+  console.log("res OUT");
+});
+
+app.get("/agent/version", (req, res, next) => {
+  logger.info('Incoming request:', {
+    method: req.method,
+    url: req.originalUrl,
+    query: req.query,
+    headers: req.headers
+  })
+
+  res.send("Sample v0.0.1");
 
   // res.setHeader("Content-type", `some/type`);
   // fs.createReadStream(filePath).pipe(res);
